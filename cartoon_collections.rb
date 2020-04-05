57d0c7e6fcf97878b223d0de68d9c907697cbd21
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-05 11:42:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index {|name, index|
-    puts "#{index + 1} #{name}"
+    puts "#{index + 1}. #{name}"
   }
 end
